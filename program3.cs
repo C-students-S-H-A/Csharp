f138fcbe68a708f38f66a884de1192170077bc1f
sina728 <sinaksina86@gmail.com>
2025-02-05 17:25:23 +0330

    Add files via upload

---
+++ program3.cs
@@ -0,0 +1,14 @@
+﻿using System;
+class program
+{
+    static void Main()
+    {
+        /*
+        Console.Write("Enter X ");
+        Console.WriteLine("Enter X");
+        */
+        Console.ReadKey();
+
+    }
+
+}
