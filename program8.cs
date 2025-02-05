6ea90c31088bdb7d53a0021bdb43e5c6b6cf1161
sina728 <sinaksina86@gmail.com>
2025-02-05 17:35:17 +0330

    Add files via upload

---
+++ program8.cs
@@ -0,0 +1,19 @@
+﻿using System;
+class program
+{
+    static void Main()
+    {
+        string first, second;
+        Console.Write("Enter First Number: ");
+        first =int.Parse(Console.ReadLine());
+        Console.Write("Enter Second Number: ");
+        second =int.Parse(Console.ReadLine());
+        Console.WriteLine(first + "+" + second + "=" + (first + second));
+        Console.WriteLine(first + "-" + second + "=" + (first - second));
+        Console.WriteLine(first + "*" + second + "=" + (first * second));
+        Console.WriteLine(first + "/" + second + "=" + (first / second));
+        Console.ReadKey();
+
+    }
+
+}
