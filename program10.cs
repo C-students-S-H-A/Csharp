9a16fa5b6340311b13c279c59528b6cc05b6385b
sina728 <sinaksina86@gmail.com>
2025-02-05 17:21:07 +0330

    Add files via upload

---
+++ program10.cs
@@ -0,0 +1,17 @@
+﻿using System;
+class program
+{
+    static void Main()
+    {
+        int X, Z, result;
+        Console.Write("Enter X: ");
+        X=int.Parse(Console.ReadLine());
+        Console.Write("Enter Z: ");
+        Z=int.Parse(Console.ReadLine());
+        result = (8 + (X * X)) - (Z * 2);
+        Console.WriteLine(result);
+        Console.ReadKey();
+
+    }
+
+}
